cb4ae3713fc00af495f25df051f12810bbbfcaf8
Jesper Petersson <jesper@jpetersson.se>
2014-07-05 20:51:16 +0200

    Made the client into an own package

--- client/client.go
+++ client/client.go
@@ -1,4 +1,4 @@
-package booliApi
+package client
 
 import (
 	"fmt"
@@ -13,43 +13,60 @@ import (
 )
 
 type Client struct {
-	Hostname, CallerId, ApiKey string
+	hostname, callerId, apiKey string
 }
 
-func (this *Client) GetHash (time, unique string) string {
+func New(hostname, callerId, apiKey string) *Client {
+	return &Client{
+		hostname: hostname,
+		callerId: callerId,
+		apiKey: apiKey,
+	}
+}
+
+func (this *Client) hash (time, unique string) string {
 	hash := sha1.New()
-	io.WriteString(hash, this.CallerId + time + this.ApiKey + unique)
+	io.WriteString(hash, this.callerId + time + this.apiKey + unique)
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
 
-func (this *Client) GetUrl (endpoint string, params map[string]string) string {
-	time := GetTime()
-	unique := GetUnique()
-	hash := this.GetHash(time, unique)
+func (this *Client) url (endpoint string, params map[string]string) string {
+	time := this.time()
+	unique := this.unique()
+	hash := this.hash(time, unique)
 
 	p := url.Values{}
 	for k, v := range params {
 		p.Set(k, v)
 	}
-	p.Set("callerId", this.CallerId)
+	p.Set("callerId", this.callerId)
 	p.Set("time", time)
 	p.Set("unique", unique)
 	p.Set("hash", hash)
 
 	query := p.Encode()
 
-	return "http://" + this.Hostname + "/" + endpoint + "?" + query
+	return "http://" + this.hostname + "/" + endpoint + "?" + query
+}
+
+func (this *Client) time() string {
+	return fmt.Sprintf("%d", time.Now().Unix())
+}
+
+func (this *Client) unique() string {
+	random := rand.New(rand.NewSource(time.Now().UnixNano())).Uint32()
+	return fmt.Sprintf("%d", random)
 }
 
 func (this *Client) Get(endpoint string, optionalParams ...map[string]string) ([]byte, error) {
 	var params map[string]string
-	if(len(optionalParams) > 0) {
+	if len(optionalParams) > 0 {
 		params = optionalParams[0]
 	} else {
 		params = map[string]string {}
 	}
 
-	url := this.GetUrl(endpoint, params)
+	url := this.url(endpoint, params)
 
 	client := http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
@@ -57,7 +74,7 @@ func (this *Client) Get(endpoint string, optionalParams ...map[string]string) ([
 
 	response, err := client.Do(req)
 
-	if(err != nil) {
+	if err != nil {
 		return nil, errors.New("Could make GET request to " + url)
 	}
 
@@ -65,13 +82,4 @@ func (this *Client) Get(endpoint string, optionalParams ...map[string]string) ([
 	body, _ := ioutil.ReadAll(response.Body)
 
 	return body, nil
-}
-
-func GetTime() string {
-	return fmt.Sprintf("%d", time.Now().Unix())
-}
-
-func GetUnique() string {
-	random := rand.New(rand.NewSource(time.Now().UnixNano())).Uint32()
-	return fmt.Sprintf("%d", random)
 }
