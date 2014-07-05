8c076bf916b4bba3e63159ae75731a95b541e0e2
Jesper Petersson <jesper@jpetersson.se>
2014-07-05 17:37:03 +0200

    Wrote a first simple version of the client

---
+++ client.go
@@ -0,0 +1,69 @@
+package booliApi
+
+import (
+	"fmt"
+	"errors"
+	"time"
+	"io"
+	"crypto/sha1"
+	"io/ioutil"
+	"math/rand"
+	"net/url"
+	"net/http"
+)
+
+type Client struct {
+	Hostname, CallerId, ApiKey string
+}
+
+func (this *Client) GetHash (time, unique string) string {
+	hash := sha1.New()
+	io.WriteString(hash, this.CallerId + time + this.ApiKey + unique)
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func (this *Client) GetUrl (endpoint string, params map[string]string) string {
+	time := GetTime()
+	unique := GetUnique()
+	hash := this.GetHash(time, unique)
+
+	p := url.Values{}
+	for k, v := range params {
+		p.Set(k, v)
+	}
+	p.Set("callerId", this.CallerId)
+	p.Set("time", time)
+	p.Set("unique", unique)
+	p.Set("hash", hash)
+
+	query := p.Encode()
+
+	return "http://" + this.Hostname + "/" + endpoint + "?" + query
+}
+
+func (this *Client) Get(endpoint string, params map[string]string) ([]byte, error) {
+	u := this.GetUrl(endpoint, params)
+
+	response, err := http.Get(u)
+	if(err != nil) {
+		return nil, errors.New("Could make GET request to " + u)
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if(err != nil) {
+		return nil, errors.New("Could parse body from " + u)
+	}
+
+	return body, nil
+}
+
+func GetTime() string {
+	return fmt.Sprintf("%d", time.Now().Unix())
+}
+
+func GetUnique() string {
+	random := rand.New(rand.NewSource(time.Now().UnixNano())).Uint32()
+	return fmt.Sprintf("%d", random)
+}
