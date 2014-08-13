1cdbd785f53f91511e53725df25040a28214db63
Jesper Petersson <jesper@jpetersson.se>
2014-08-13 09:25:45 +0200

    Removed unused dependency in previous sales

--- client/previous_sales.go
+++ client/previous_sales.go
@@ -1,14 +1,10 @@
 package client
 
-import (
-	"fmt"
-)
-
 func (this *Client) PreviousSales(_ int, optionalParams ...map[string]string) ([]byte, error) {
 	params := map[string]string{}
 	if len(optionalParams) > 0 {
 		params = optionalParams[0]
 	}
 
-	return this.Get(fmt.Sprintf("sold"), params)
+	return this.Get("sold", params)
 }
